019a5999bdf08d1fbd2580f24e545a16ee40ca71
eduarda.cunha <eduardaesfrecula1@gmail.com>
2026-03-06 12:21:21 -0300

    Adicionando main do portfólio em Streamlit

---
+++ ProjetoExtencao.py
@@ -0,0 +1,24 @@
+import sqlite3 as sql
+
+con = sql.connect('form_db.db')
+cur = con.cursor()
+
+cur.execute('DROP TABLE IF EXISTS users')
+
+sql_create = '''
+CREATE TABLE users (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    NOME TEXT,
+    IDADE TEXT,
+    CEP TEXT,
+    RUA TEXT,
+    NUMERO TEXT,
+    ESTADO TEXT,
+    EMAIL TEXT
+)
+'''
+
+cur.execute(sql_create)
+
+con.commit()
+con.close()
--- ProjetoExtencao/Sistemaos.PY
+++
@@ -1,2 +0,0 @@
-import sqlite as sql
-
